6f6fb2f98a31165e610179db23570baa2135d0a9
Alpha <alphagma@gmail.com>
2015-03-06 20:45:56 -0600

    Added git to required libraries in bootstraping script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update -y > /dev/null
 export DEBIAN_FRONTEND=noninteractive
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password '
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password '
-apt-get install -y curl nodejs mysql-server-5.5 mysql-client-5.5 libmysqlclient-dev > /dev/null
+apt-get install -y git curl nodejs mysql-server-5.5 mysql-client-5.5 libmysqlclient-dev > /dev/null
 apt-get install -y imagemagick --fix-missing
 curl -sSL https://get.rvm.io | bash -s stable --rails --quiet-curl
 source /usr/local/rvm/scripts/rvm
